3e9b4cbd7e7014da1f8d7a605470baa9f6ae2eb9
Mostafa Ali <moustafakhameis@gmail.com>
2025-09-23 11:56:52 +0300

    Update brands fetch to use force-cache to fix dynamic server error

--- src/app/_services/brands.service.ts
+++ src/app/_services/brands.service.ts
@@ -7,7 +7,7 @@ const BASE_URL = "https://ecommerce.routemisr.com/api/v1/brands";
 
 export async function getAllBrands(): Promise<BrandType[]> {
   try {
-    const res = await fetch(BASE_URL, { cache: "no-store" });
+    const res = await fetch(BASE_URL, { cache: "force-cache" });
     if (!res.ok) throw new Error("Failed to fetch brands");
     const data = await res.json();
     return data.data;
@@ -20,7 +20,7 @@ export async function getAllBrands(): Promise<BrandType[]> {
 
 export async function getBrandById(id: string): Promise<BrandType | null> {
   try {
-    const res = await fetch(`${BASE_URL}/${id}`, { cache: "no-store" });
+    const res = await fetch(`${BASE_URL}/${id}`, { cache: "force-cache" });
     if (!res.ok) throw new Error("Failed to fetch brand");
     const data = await res.json();
     return data.data;
